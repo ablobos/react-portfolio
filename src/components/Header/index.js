ce0ec9d14d7545fbce86b5725cd2de096973a7d0
Abdiel "A.B." Villalobos <ablobosportfolio@gmail.com>
2023-04-11 10:58:00 -0400

    header component completed

---
+++ src/components/Header/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Nav from "../Nav";
+
+function Header(props) {
+    const { currentTab, handleTabChange } = props;
+
+    return (
+        <div>
+            <section>
+                <header className="flex-row px-1">
+                    <div>
+                        <h1>Abdiel Villalobo's Portfolio</h1>
+                    </div>
+                    <div>
+                        <Nav
+                        currentTab={currentTab}
+                        handleTabChange={handleTabChange}
+                        ></Nav>
+                    </div>
+                </header>
+            </section>
+            <section class="hero">
+                <div class="hero-cta">
+                    <h2>Welcome</h2>
+                    <p>
+                        Welcome to Abdiel's Portfolio page created with React! Here, you are able to find a small collection of my work created over the course of the project.
+                    </p>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default Header;
