b209ab39b126a30ca850c4e98b63db940226c1c8
Abdiel "A.B." Villalobos <ablobosportfolio@gmail.com>
2023-04-11 13:06:58 -0400

    about component completed

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,1 +1,26 @@
 import React from "react";
+// import profileImage from "../../assets/photos/pfp.png"
+
+function About() {
+    return (
+        <section id="about-me" className="my-5 intro">
+            <div class="flex-row">
+                <h1 id="about" className="section-title primary-border">
+                    About Me
+                </h1>
+            </div>
+            <div className="flex-row">
+                <div class="intro-info">
+                    <div class="intro-img">
+                        <img src={profileImage} style={{ width: "100%" }} alt="profile" />
+                    </div>
+                    <p>
+                        Brief Introduction
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default About;
